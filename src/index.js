2a0da6f97c17f06b5201bec45241055552d13ab5
Sarah Reimann <reimannsarah45@gmail.com>
2023-04-17 13:20:34 -0700

    add print error functionality

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import './css/styles.css';
 
 function getGif(search) {
   let request = new XMLHttpRequest();
-  const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=25`;
+  const url = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=50`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
       printElements(response, search);
+      console.log(response.meta.response_id);
+    } else { printError(request, response, search);
     }
   });
   request.open("GET", url, true);
@@ -20,12 +22,16 @@ function getGif(search) {
 
 function printElements(response, search) {
   const outputDiv = document.querySelector('#output');
-  outputDiv.innerText = `your search for ${search}, yielded results`;
+  outputDiv.innerText = `your searched for: ${search}`;
   response.data.forEach((element) => {
     const img = document.createElement("img");
     img.src = element.images.original.url;
     outputDiv.append(img);
   });
+}
+
+function printError(request, response, search) {
+  document.querySelector('#output').innerText = `There was an error finding ${search}. ${request.status} ${response.meta.msg}`;
 }
 
 
